65bdbc92b96800aad25a0d5d0ded62fab8e749aa
Dominix446 <97578554+Dominix446@users.noreply.github.com>
2025-11-13 20:50:18 +0530

    Update Intel.java

--- Intel.java
+++ Intel.java
@@ -1,12 +1,24 @@
 class Intel{
- Krai[] create1(boolean cylcic,Krai... iu){//iu for intelligence unit, nominally, this create1 is/shud be used for creating the very base of a krai intelligence structure.period.
-   for(int i=0;i<iu.length;i++){
-     if(i>0){
-       iu[i].protocol=iu[i-1].protocol;
-     }
-     if(cyclic=true){
-       iu[iu.length-1].protocol=iu[0];}     
-   }
-   return iu;
-   }
- }
+  private static int __depth = 0;
+  Krai[] create1(boolean cyclic,Krai... iu){
+    for(int i=0;i<iu.length;i++){
+      if(i>0){
+        iu[i].protocol=iu[i-1].protocol.link[0];
+      }
+      if(cyclic==true){
+        iu[iu.length-1].protocol.link[0]=iu[0];
+      }
+    }
+    return iu;
+  }
+  void self_arrange(Krai[] link){
+    int n=link.length;
+    for (int i = 0; i < n - 2; i++){
+      double d = dissimilarity(link[i], link[i+1]);
+      for(int j=i;j<n;j++){
+        if(d > dissimilarity(link[j], link[i+1])){
+          link[i+1]=link[i+1].protocol_ROLARI(0,link[j].protocol);
+        }
+      }
+    }
+  }
